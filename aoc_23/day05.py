fe3d58d2061585b691b7a15657d922a868b68b3a
syzhang <syzhang@outlook.com>
2023-12-10 11:03:20 +0000

    day5b

--- aoc_23/day05.py
+++ aoc_23/day05.py
@@ -5,6 +5,7 @@ import re
 from functools import reduce
 from utils import get_input
 from tqdm import tqdm
+from math import ceil, log10
 
 
 def extract_seeds(data):
@@ -45,69 +46,46 @@ def find_mapping(seeds, mapping):
     return locations
 
 
-def brute_force_mapping(seeds, mapping):
+def brute_force_mapping(seeds, mapping, print_output=True):
     """find mapping to final"""
     new_seeds = []
     seed_start = seeds[0::2]
     seed_range = seeds[1::2]
-    min_loc = int(1e100)
-    min_seed = 0
-    min_range = 0
-    for start, seed_range in tqdm(zip(seed_start, seed_range)):
-        # check every sqrt of seed_range
-        n_steps = (start + seed_range) // 2
-        for i in range(start, start + seed_range, n_steps):
-            location = reduce(extract_mappings, mapping, i)
-            if location < min_loc:
-                min_loc = location
-                min_seed = i
-                min_range = seed_range
+    
+    # Calculate step size
+    max_seed = max(seed_range)
+    step_size = int(pow(10, ceil(log10(max_seed / 100))))
+    
+    # Generate search values
+    search_vals = generate_search_values(zip(seed_start, seed_range), step_size, mapping)
+    
+    # Get rough estimate
+    rough_est = min(search_vals.items(), key = lambda x: x[1])
+    seed_range_start, seed_range_end, best_est = rough_est[0]
 
-    # iterate over range of min_seed
-    for i in tqdm(range(min_seed, min_seed + min_range)):
-        location = reduce(extract_mappings, mapping, i)
-        if location < min_loc:
-            min_loc = location
-    return min_loc
+    print(f'Best estimate: {best_est} in seed range {seed_range_start} to {seed_range_end}')
+    print(f'Step size: {step_size:<8d}, best estimate: {best_est:<10d} near location {rough_est[1]}')
 
+    while step_size > 1:
+        left_search  = max(best_est - step_size, seed_range_start)
+        right_search = min(best_est + step_size, seed_range_end)
 
-def extract_mappings_b(inputs, mapping):
-    """extract mappings from input"""
-    for start, seed_range in inputs:
-        while seed_range > 0:
-            for m in mapping.split("\n")[1:]:
-                # extract data
-                if not m:
-                    continue
-                destination, source, range_len = m.split()
-                # convert to int
-                destination = int(destination)
-                source = int(source)
-                range_len = int(range_len)
-                # find offset
-                delta = start - source
-                if delta in range(range_len):
-                    range_len = min(range_len - delta, seed_range)
-                    yield (destination + delta, range_len)
-                    start += range_len
-                    seed_range -= range_len
-                    break
-            else:
-                return (start, range_len)
+        step_size = step_size // 10
+        search_vals = {s: find_mapping([s], mapping=mapping) for s in range(left_search, right_search, step_size)}
+        best_est, best_loc = min(search_vals.items(), key = lambda x: x[1])
 
+        if print_output:
+            print(f'Step size: {step_size:<8d}, best estimate: {best_est:<10d} near loc {best_loc}')
 
-def find_mapping_b(seeds, mapping):
-    """find mapping to final"""
-    seed_start = seeds[0::2]
-    seed_range = seeds[1::2]
-    pairs = zip(seed_start, seed_range)
-    locations = []
-    for s in pairs:
-        locs = list(reduce(extract_mappings_b, mapping, [s]))  # get a list of mappings
-        if locs:  # check if locs is not empty
-            min_loc = min(locs)
-            locations.append(min_loc[0])
-    return locations
+    return best_loc
+
+def generate_search_values(seeds, step_size, mapping):
+    """Generate search values using seeds, step size and maps"""
+    search_vals = {}
+    for ss, sl in seeds:
+        for s in range(ss, ss + sl, step_size):
+            search_vals[(ss, ss + sl, s)] = find_mapping(seeds=[s], mapping=mapping)
+    return search_vals
 
 
 # main
@@ -124,6 +102,5 @@ if __name__ == "__main__":
     # print(min(locations))
 
     # part b
-    # locations = find_mapping_b(seeds, mappings)
     min_loc = brute_force_mapping(seeds, mappings)
     print(min_loc)
